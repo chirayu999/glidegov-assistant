5bb11add987afaa61a819039471a7eb5ee76a07e
chirayu999 <chirayu.vithalani45@gmail.com>
2026-03-02 23:17:48 +0530

    routes

---
+++ backend/config/routes.rb
@@ -0,0 +1,22 @@
+require "sidekiq/web"
+
+Rails.application.routes.draw do
+  namespace :api do
+    resources :sessions, only: [:create, :show, :update] do
+      resources :turns, only: [:index, :create], controller: "conversation_turns"
+    end
+
+    post "schemes/discover", to: "schemes#discover"
+    post "eligibility/check", to: "eligibility#check"
+    post "form_navigation/start", to: "form_navigation#start"
+    post "form_navigation/submit_data", to: "form_navigation#submit_data"
+    post "form_navigation/submit_otp", to: "form_navigation#submit_otp"
+    get  "form_navigation/status", to: "form_navigation#status"
+  end
+
+  mount ActionCable.server => "/cable"
+
+  if Rails.env.development?
+    mount Sidekiq::Web => "/sidekiq"
+  end
+end
